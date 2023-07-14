d70c4c05173088a272fe1bd1d872d6a2b4d46acf
ioannisPApapadopoulos <john.papadopoulos0@gmail.com>
2023-07-14 19:33:14 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,6 @@ using Test, RadialPiecewisePolynomials
 
 include("test_diskelement.jl")
 include("test_annuluselement.jl")
+include("test_finiteannulusmode.jl")
+include("test_finiteannulus.jl")
+include("test_finitezernike.jl")
